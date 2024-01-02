d183a67a9d21a1ee98ed3aefb6901a08a2c6a625
Juan <jpcuomo2000@gmail.com>
2024-01-02 19:03:16 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio I02 - Cotizador/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio_I02___Cotizador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
